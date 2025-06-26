8203a28848ac6cdded85271221544975e12a09de
k-trash <k.trash.t@gmail.com>
2025-06-26 14:10:10 +0900

    fix : vim keyCode

--- k-trash_setting.sh
+++ k-trash_setting.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo -e "{\n\t"keyboard.dispatch": "keyCode"\n}" > $HOME/.config/Code/User/settings.json
+echo -e '{\n\t"keyboard.dispatch": "keyCode"\n}' > $HOME/.config/Code/User/settings.json
 touch $HOME/.vimrc
 echo -e "set number\nset smartindent" >> $HOME/.vimrc
 
