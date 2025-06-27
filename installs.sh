c704c6587499a6091ae70622741622464ac3a7dd
k-trash <76803639+k-trash@users.noreply.github.com>
2025-06-27 13:46:32 +0900

    Update installs.sh

--- installs.sh
+++ installs.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 #remove games
-apt remove -y gnome-sudoku gnome-mines gnome-mahjongg aisleriot
+sudo apt remove -y gnome-sudoku gnome-mines gnome-mahjongg aisleriot
 
 #update & uprade
-apt -y update
-apt -y upgrade
+sudo apt -y update
+sudo apt -y upgrade
 
 #remove firefox
-apt remove -y firefox* thunderbird
-snap remove firefox
+sudo apt remove -y firefox* thunderbird
+sudo snap remove firefox
 
 #install gufw
-apt install -y gufw
-ufw enable
+sudo apt install -y gufw
+sudo ufw enable
 
 #install chrome
 #mkdir -p $HOME/Downloads/install_file
@@ -22,20 +22,20 @@ ufw enable
 #rm -f $HOME/Downloads/install_file/google-chrome-stable_current_amd64.deb
 
 #install utilities
-apt install -y gnome-tweaks
-apt install -y curl apt-transport-https ca-certificates gnupg ssh
-apt install -y git build-essential cmake vim
+sudo apt install -y gnome-tweaks
+sudo apt install -y curl apt-transport-https ca-certificates gnupg ssh
+sudo apt install -y git build-essential cmake vim
 
 #install docker
-install -m 0755 -d /etc/apt/keyrings
+sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 #install vscode
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > $HOME/Downloads/install_file/microsoft.gpg
@@ -52,10 +52,10 @@ if [ $? -gt 0 ]; then
 	read -p 'Remove all existing nvidia-driver and cuda-toolkit. (y/N): ' yn
 	case "$yn" in
 		[yY]*) 
-			apt remove --purge nvidia-* cuda-*
-			apt install -y ubuntu-drivers-common
+			sudo apt remove --purge nvidia-* cuda-*
+			sudo apt install -y ubuntu-drivers-common
 			driver=$(ubuntu-drivers devices | grep recommended | awk '{print $3}')
-			apt install -y --no-install-recommends ${driver}
+			sudo apt install -y --no-install-recommends ${driver}
 			echo 'please reboot and continue installation of cuda-toolkit by running setup_after.bash';;
 		*)
 			echo 'abort installing nvidia-drivers';;
